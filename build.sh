08ad04e0adc9eca74043413f3c8c8d8cc52c2817
Josh Zarrabi <jzarrabi@pivotal.io>
2019-03-01 09:47:40 -0500

    allow operator to configure socket endpoint
    [#164306350]

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 mkdir releases
-aws s3 cp "s3://ch-releases/sample-credhub-kms-plugin-0.0.2.tgz" releases/
+aws s3 cp "s3://ch-releases/sample-credhub-kms-plugin-0.0.3.tgz" releases/
 tile build
