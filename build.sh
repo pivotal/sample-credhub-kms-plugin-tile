98f79abdad4c002a09fb31626a659845152c5622
Andrew Keesler <akeesler@pivotal.io>
2020-04-06 12:50:48 -0400

    build.sh: use gsutil since our blobs moved aws -> gcp.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 mkdir releases
-aws s3 cp "s3://ch-releases/sample-credhub-kms-plugin-0.0.5.tgz" releases/
+gsutil cp "gs://ch-releases/sample-credhub-kms-plugin-0.0.5.tgz" releases/
 tile build
